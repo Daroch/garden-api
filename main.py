cbd677e7c9d993732822dc37add23fdf57d4f5ca
Daroch <daroch314@gmail.com>
2023-10-31 12:34:08 +0100

    adding DB connection and first @app route with api call to get all plants

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ from models import Base
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-
+def get_db():
+  db = localSession()
+  try:
+    yield db
+  finally:
+    db.close()
+    
 @app.get('/')
 def root():
     return 'Hello, I am FastAPI'
+
+@app.get('/api/plants', response_model=list[PlantId])
+def get_plants(db: Session = Depends(get_db)):
+  return crud.get_plants(db)
+  
+  
