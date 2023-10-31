10c64b998182beb9a7fbcc3f8edf86e45c15bb71
Daroch <daroch314@gmail.com>
2023-10-31 02:03:40 +0100

    Create model for table plants with basic name, description and publish fileds

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer, Enum, DATETIME, Boolean
+
+from database import Base
+
+class Plant(Base):
+  __tablename__ = 'plants'
+  
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String(30), index=True)
+  description = Column(String(300))
+  publish = Column(bool)
