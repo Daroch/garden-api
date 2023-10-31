745ec2d001b462dcd0e540d3f762f28542ae5b7f
Daroch <daroch314@gmail.com>
2023-10-31 02:24:33 +0100

    import required python modules from other files

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+from database import engine, localSession
+from schemas import PlantData, PlantId
+from models import Base
 
 app = FastAPI()
 
+
 @app.get('/')
 def root():
-  return 'Hello, I am FastAPI'
+    return 'Hello, I am FastAPI'
