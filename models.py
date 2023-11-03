49f4743e5a21ae304a66a7b225b46bfc54993ceb
Daroch <daroch314@gmail.com>
2023-11-03 16:31:35 +0100

    added User database model

--- models.py
+++ models.py
@@ -2,6 +2,15 @@ from sqlalchemy import Column, String, Integer, Enum, DATETIME, Boolean
 
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    plants = relationship("Plant", back_populates="owner")
 class Plant(Base):
   __tablename__ = 'plants'
   
@@ -9,3 +18,6 @@ class Plant(Base):
   name = Column(String(30), index=True)
   description = Column(String(300))
   publish = Column(Boolean, default=False)
+  owner_id = Column(Integer, ForeignKey("users.id"))
+  
+  owner = relationship("User", back_populates="plants")
