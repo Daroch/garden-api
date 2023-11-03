308328e9db30102c9927b6885981899c28454bac
Daroch <daroch314@gmail.com>
2023-11-03 16:32:06 +0100

    Added User Pydantyc model (schema)

--- schemas.py
+++ schemas.py
@@ -1,5 +1,19 @@
 from pydantic import BaseModel
 
+class UserBase(BaseModel):
+    email: str
+    
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    plants: list[Plant] = []
+
+    class Config:
+        orm_mode = True
+        
 class PlantData(BaseModel):
   name: str
   description: str
@@ -7,3 +21,4 @@ class PlantData(BaseModel):
 
 class PlantId(PlantData):
   id: int
+  
