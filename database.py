44ed7fd26839b9e4b5624379193ed099583bacd6
Daroch <daroch314@gmail.com>
2023-11-07 12:44:06 +0100

    create function to select between test and real database

--- database.py
+++ database.py
@@ -3,16 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_NAME=os.getenv('DB_NAME')
+_DB_NAME=os.getenv('DB_NAME')
 DB_HOST=os.getenv('DB_HOST')
 DB_PASSWORD=os.getenv('DB_PASSWORD')
 DB_DIALECT=os.getenv('DB_DIALECT')
 DB_USER=os.getenv('DB_USER')
 
-print(os.getenv('DB_DIALECT'))
-URL_CONNECTION = '{}://{}:{}@{}/{}'.format(DB_DIALECT,DB_USER,DB_PASSWORD,DB_HOST,DB_NAME)
+def db_name():
+    if os.getenv('RUN_ENV') == 'test':
+        return 'test_' + _DB_NAME
+
+    return _DB_NAME
+      
+      
+URL_CONNECTION = '{}://{}:{}@{}/{}'.format(DB_DIALECT,DB_USER,DB_PASSWORD,DB_HOST,db_name())
 engine = create_engine(URL_CONNECTION)
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
 Base = declarative_base()
-print('hola')
