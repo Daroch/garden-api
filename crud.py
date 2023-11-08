12a672c1a26d96439a1e1bec8d27fc8de2847b7a
Daroch <daroch314@gmail.com>
2023-11-08 02:50:18 +0100

    fixing update crud

--- crud.py
+++ crud.py
@@ -1,23 +1,29 @@
 from sqlalchemy.orm import Session
 
 from models import User, Plant, Category
-from schemas import UserCreate, PlantCreate, CategoryCreate, Plant as SchemaPlant, Category as SchemaCategory 
+from schemas import UserCreate, PlantCreate, CategoryCreate, Plant as SchemaPlant, Category as SchemaCategory
+
 
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
 
+
 def get_user_by_name(db: Session, name: str):
     return db.query(User).filter(User.name == name).first()
+
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(User).offset(skip).limit(limit).all()
 
+
 def create_user(db: Session, user: UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
-    db_user = User(email=user.email, name=user.name, hashed_password=fake_hashed_password)
+    db_user = User(email=user.email, name=user.name,
+                   hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -25,10 +31,16 @@ def create_user(db: Session, user: UserCreate):
 
 
 def get_plant(db: Session, plant_id: int):
-   return db.query(Plant).filter(Plant.id == plant_id).first()
+    return db.query(Plant).filter(Plant.id == plant_id).first()
+
 
 def get_plants(db: Session, skip: int = 0, limit: int = 100):
-   return db.query(Plant).offset(skip).limit(limit).all()
+    return db.query(Plant).offset(skip).limit(limit).all()
+
+
+def get_plants_for_user(db: Session, user_id: int, skip: int = 0, limit: int = 100):
+    return db.query(Plant).filter(Plant.owner_id == user_id).offset(skip).limit(limit).all()
+
 
 def create_user_plant(db: Session, plant: PlantCreate, user_id: int, category_id: int):
     db_plant = Plant(**plant.dict(), owner_id=user_id, category_id=category_id)
@@ -37,22 +49,27 @@ def create_user_plant(db: Session, plant: PlantCreate, user_id: int, category_id
     db.refresh(db_plant)
     return db_plant
 
-def update_user_plant(db: Session, plant: SchemaPlant):
-    
-    db_plant = Plant(**plant.dict())
-    print(db_plant)
+
+def update_user_plant(db: Session, plant_id: int, plant: PlantCreate):
+    db_plant = get_plant(db, plant_id=plant_id)
+    for key, value in plant:
+        setattr(db_plant, key, value) 
+    #db_plant = Plant(**plant.dict())
     db.commit()
-    db.refresh(db_plant)
     return db_plant
 
+
 def get_categories(db: Session, skip: int = 0, limit: int = 100):
-   return db.query(Category).offset(skip).limit(limit).all()
+    return db.query(Category).offset(skip).limit(limit).all()
+
 
 def get_category_by_name(db: Session, name: str):
     return db.query(Category).filter(Category.name == name).first()
 
+
 def create_category(db: Session, category: CategoryCreate):
-    db_category = Category(name=category.name, description=category.description)
+    db_category = Category(
+        name=category.name, description=category.description)
     db.add(db_category)
     db.commit()
     db.refresh(db_category)
--- main.py
+++ main.py
@@ -44,6 +44,12 @@ def create_plant_for_user(
     return crud.create_user_plant(db=db, plant=plant, user_id=user_id, category_id=category_id)
 
 
+@app.get("/users/{user_id}/plants", response_model=list[schemas.Plant])
+def read_plants_for_user(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    plants = crud.get_plants_for_user(db, skip=skip, limit=limit, user_id=user_id)
+    return plants
+
+
 @app.get("/users/{user_id}/plants/{plant_id}", response_model=schemas.Plant)
 def read_plant(
     plant_id: int, db: Session = Depends(get_db)
@@ -55,14 +61,14 @@ def read_plant(
 
 
 @app.patch("/users/{user_id}/plants/{plant_id}", response_model=schemas.Plant)
-def update_plant_for_user(user_id: int, plant_id: int, plant: schemas.Plant, db: Session = Depends(get_db)
+def update_plant_for_user(user_id: int, plant_id: int, plant: schemas.PlantCreate, db: Session = Depends(get_db)
 ):
     db_plant = crud.get_plant(db, plant_id=plant_id)
     if  db_plant is None:
         raise HTTPException(status_code=404, detail="Plant not found")
-    if  user_id == 2:
+    if  user_id is not db_plant.owner_id:
         raise HTTPException(status_code=500, detail="This is not your plant")
-    return crud.update_user_plant(db=db, plant=plant)
+    return crud.update_user_plant(db=db, plant_id=plant_id, plant=plant)
 
 
 
