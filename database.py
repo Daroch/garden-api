12f9023bb7b460a4eed18d08fc101a97ddc5d700
Daroch <daroch314@gmail.com>
2023-11-09 18:21:20 +0100

    update import declarative_base directly from sqlalchemy.orm

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 _DB_NAME=os.getenv('DB_NAME')
 DB_HOST=os.getenv('DB_HOST')
