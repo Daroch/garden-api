5408bd003ee307849006f7a7b467e0819688f155
Daroch <daroch314@gmail.com>
2023-11-10 00:38:30 +0100

    testing create user with duplicate email

--- test/test_users.py
+++ test/test_users.py
@@ -8,7 +8,7 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello, I am FastAPI"}
 
-def test_create_user():
+def test_create_user_ok():
     user = {
   "email": "frodo@gmail.com",
   "name": "Frodo",
@@ -19,3 +19,14 @@ def test_create_user():
     data = response.json()
     assert data['email'] == user['email']
     assert data['name'] == user['name']
+
+def test_create_user_duplicate():
+    user = {
+  "email": "frodo@gmail.com",
+  "name": "Frodo",
+  "password": "12345"
+}
+    response = client.post("/users/",json=user)
+    assert response.status_code == 400, response.text
+    data = response.json()
+    assert response.json() == {"detail": "Email already registered"}
