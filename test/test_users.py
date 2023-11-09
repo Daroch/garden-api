6c5c1ab88d81333ac32f0b4fd757edbf53518dae
Daroch <daroch314@gmail.com>
2023-11-09 19:05:18 +0100

    test create user

--- test/test_users.py
+++ test/test_users.py
@@ -7,3 +7,15 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello, I am FastAPI"}
+
+def test_create_user():
+    user = {
+  "email": "frodo@gmail.com",
+  "name": "Frodo",
+  "password": "12345"
+}
+    response = client.post("/users/",json=user)
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data['email'] == user['email']
+    assert data['name'] == user['name']
