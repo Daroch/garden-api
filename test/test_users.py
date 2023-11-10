9638c391a7bb4ecdf89a5745b8a7184ab6f2a46d
Daroch <daroch314@gmail.com>
2023-11-10 13:45:59 +0100

    testing user case

--- test/test_users.py
+++ test/test_users.py
@@ -19,6 +19,7 @@ def test_create_user_ok():
     data = response.json()
     assert data['email'] == user['email']
     assert data['name'] == user['name']
+    response2 = client.delete(f"users/{data['id']}")
 
 def test_create_user_duplicate():
     user = {
@@ -27,6 +28,10 @@ def test_create_user_duplicate():
   "password": "12345"
 }
     response = client.post("/users/",json=user)
-    assert response.status_code == 400, response.text
+    assert response.status_code == 201, response.text
     data = response.json()
+    response = client.post("/users/",json=user)
+    assert response.status_code == 400, response.text
     assert response.json() == {"detail": "Email already registered"}
+    response = client.delete(f"users/{data['id']}")
+
